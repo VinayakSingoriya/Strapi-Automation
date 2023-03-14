90476130b31c30723bfc6a79e219393892f3642e
Vinayak Singoriya <vinayaksingoriya@gmail.com>
2023-03-15 01:29:21 +0530

    credential secured

--- scripts/pullCode.sh
+++ scripts/pullCode.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 echo '>> PULLING STRAPI APP SOURCE CODE .....'
-export GIT_USERNAME=VinayakSingoriya
-export GIT_PASSWORD=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
+export GIT_USERNAME=
+export GIT_PASSWORD=
 git clone https://$GIT_USERNAME:$GIT_PASSWORD@github.com/VinayakSingoriya/Strapi-Demo-App.git strapiApp
 echo '<<   STRAPI APP SOURCE CODE PULLED  >>'
 
